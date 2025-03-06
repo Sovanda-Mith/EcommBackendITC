f723645ac5132409249a111bc202de8a4825f8e2
sovanda_m <vanda.can.read@gmail.com>
2025-03-06 21:53:48 +0700

    finished TP6

---
+++ laravel/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function getCategories()
+    {
+        $categories = Category::all();
+        return response()->json(['categories' => $categories]);
+    }
+
+    public function createCategory(Request $request)
+    {
+        $validated = $request->validate(
+            ["name" => "required|unique:categories,name"]
+        );
+
+        $category = Category::create(["name" => $validated['name']]);
+
+        return response()->json(["message" => "Category " . $validated['name'] . " created"]);
+    }
+
+    public function getCategory($categoryId)
+    {
+        try {
+            $category = Category::findOrFail($categoryId);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(["message" => "Category not found"], 404);
+        }
+
+        return response()->json($category);
+    }
+
+    public function updateCategory($categoryId, Request $request)
+    {
+        $validated = $request->validate(
+            ["name" => "required|unique:categories,name"]
+        );
+
+        try {
+            $category = Category::findOrFail($categoryId);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(["message" => "Category not found"], 404);
+        }
+
+        $category->name = $validated['name'];
+        $category->save();
+
+        return response()->json(["message" => "Category " . $validated['name'] . " updated"]);
+    }
+
+    public function deleteCategory($categoryId)
+    {
+        try {
+            $category = Category::findOrFail($categoryId);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(["message" => "Category not found"], 404);
+        }
+
+        $category->delete();
+
+        return response()->json(["message" => "Category deleted"]);
+    }
+}
---
+++ laravel/app/Http/Controllers/ProductController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function getProducts()
+    {
+        $products = Product::all();
+
+        return response()->json(["products" => $products]);
+    }
+
+    public function createProduct(Request $request)
+    {
+        $validated = $request->validate([
+            "name" => "required|string|unique:products,name",
+            "category_id" => "required|exists:categories,id",
+            "description" => "nullable|string",
+            "pricing" => "required|numeric",
+            "images" => "nullable|json"
+        ]);
+
+        $newProduct = new Product();
+
+        $newProduct->name = $validated['name'];
+        $newProduct->category_id = $validated['category_id'];
+        $newProduct->description = $validated['description'];
+        $newProduct->pricing = $validated['pricing'];
+        $newProduct->images = $validated['images'];
+
+        $newProduct->save();
+
+        return response()->json(["message" => "Product created"]);
+    }
+
+    public function getProduct($productId)
+    {
+        try {
+            $product = Product::findOrFail($productId);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(["message" => "Product not found"], 404);
+        }
+
+        return response()->json(["product" => $product]);
+    }
+
+    public function updateProduct($productId, Request $request)
+    {
+        $validated = $request->validate([
+            "name" => "required|string|unique:products,name",
+            "category_id" => "required|exists:categories,id",
+            "description" => "nullable|string",
+            "pricing" => "required|numeric",
+            "images" => "nullable|json"
+        ]);
+
+        try {
+            $product = Product::findOrFail($productId);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(["message" => "Product not found"], 404);
+        }
+
+        $product->name = $validated['name'];
+        $product->category_id = $validated['category_id'];
+        $product->description = $validated['description'];
+        $product->pricing = $validated['pricing'];
+        $product->images = $validated['images'];
+
+        $product->save();
+
+        return response()->json(["message" => "Product updated"]);
+    }
+
+    public function deleteProduct($productId)
+    {
+        try {
+            $product = Product::findOrFail($productId);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(["message" => "Product not found"], 404);
+        }
+
+        $product->delete();
+
+        return response()->json(["message" => "Product deleted"]);
+
+    }
+
+    public function getProductsByCategory($categoryId)
+    {
+        $products = Product::where('category_id', $categoryId)->get();
+
+        return response()->json(["products" => $products]);
+    }
+}
