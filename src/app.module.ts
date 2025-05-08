b99c242b3b0ab5cc662de8f931eb0220f213ed28
sovanda_m <vanda.can.read@gmail.com>
2025-05-08 14:52:39 +0700

    Step3 without real implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { TaskModule } from './modules/task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './tasks/task.entity';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [UserModule, TaskModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'todo.sqlite',
+      entities: [User, Task],
+      synchronize: true,
+    }),
+    UserModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
